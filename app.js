630506498795065a12b08d1dbb3d0ba5eed0f052
dga <dga@infogones.com>
2016-08-13 12:32:18 +0100

    avancement dans le tp, ajout du formulaire, mais jai tout casse

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(session({secret: 'plop'}));
 
 // If todolist doesn't exist, create an empty one
 app.use(function(req, res, next){
-    if (typeof(req.session.todolist) == 'undefined') {
+  
+    if (typeof(req.session.todolist) === 'undefined') {
         req.session.todolist = [];
     }
     next();
@@ -20,10 +21,18 @@ app.get ('/', function(req, res) {
         res.render('home.jade', {session: req.session});
     })
     .get('/todo', function(req,res) {
-        res.render('todo.jade', {todolist : req.session.todolist});
+        res.render('todo_view.jade', {todolist : req.session.todolist});
     })
     .post('/todo/add', function(req, res) {
+        // if form has been filled, add task to session
+        if (typeof(req.task) !== 'undefined') {
+            req.session.todolist.push(req.task.label);
+        } 
         
+        console.log(req.session.todolist);
+        // In any case, display todolist + form 
+        res.render('todo_view.jade', {todolist: req.session.todolist});
+          
     })
     .get('/todo/delete/:id', function(req,res) {
         
